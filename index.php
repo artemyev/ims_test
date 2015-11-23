cceaee04810709b501f8dba073a7a8948d70fb80
Evgeniy Artemyev <artemyev@users.noreply.github.com>
2015-11-23 17:20:18 +0300

    Update index.php

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ if(!include('config.php')){
 @header('Last-Modified: '.gmdate("D, d M Y H:i:s").' GMT');
 @header('Content-Type: text/html; charset=utf-8');
 ?>
-<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\" \"http://www.w3.org/TR/html4/loose.dtd\">
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
